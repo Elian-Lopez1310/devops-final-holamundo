c16c11d7bd04baf6a2d3055ffbbb99b3a4df0358
Elian-Lopez1310 <elianstiven055@gmail.com>
2025-12-09 18:02:20 -0400

    Disparar pipeline CI/CD

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo DevOps CI/CD 🚀");
+res.send("Hola Mundo desde DevOps AUTOMÁTICO 🚀");
+
 });
 
 module.exports = app;
